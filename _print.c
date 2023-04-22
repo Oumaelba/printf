67cc634888556475bafee4dd07c6ae759fed2c16
yghari <ghariyouness90@gmail.com>
2023-04-22 15:54:30 +0000

    fixing some errors

--- _print.c
+++ _print.c
@@ -44,7 +44,9 @@ int _printf(const char *format, ...)
 	i = 0;
 	tmpi = 0;
 	len = 0;
-	if (format == NULL)
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 	if (format != NULL)
 	{
