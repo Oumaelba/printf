eca8d106d8346b2f837e539e723501629fec66fb
yghari <ghariyouness90@gmail.com>
2023-04-26 00:25:20 +0100

    potential cases fix

--- non_printabl.c
+++ non_printabl.c
@@ -1,57 +1,65 @@
 #include "main.h"
+#include <limits.h>
 
 /**
- * char_to_upper_hex - convert num to hex lower
+ * char_to_upper_hex - convert num to hex upper
  * @num: the number
- * @len: pointer to the string length
  *
- * Return: 0 on succes, -1 on error
+ * Return: length of the hex string on success, -1 on error
  */
-int	char_to_upper_hex(int num, int *len)
+int char_to_upper_hex(int num)
 {
-	int i, size = 2;
-	char *hex;
+	int i, size = 2, len = 2;
+	char *hex, *new_hex;
 	char *up = "0123456789ABCDEF";
 
 	hex = malloc(size * sizeof(char));
 	if (!hex)
-	{
-		free(hex);
 		return (-1);
-	}
-
 	if (num <= 16)
 	{
-		*len += _printf("0%c", up[num]);
-		return (0);
+		_printf("0%c", up[num]);
+		free(hex);
+		return (2);
 	}
 	else
 	{
 		for (i = 0; num != 0; i++)
 		{
 			hex[i] = up[num % 16];
-			hex = _realloc(hex, size * sizeof(char), (size + 1) * sizeof(char));
+			if (size >= INT_MAX / sizeof(char))
+			{
+				free(hex);
+				return (-1);
+			}
+			new_hex = _realloc(hex, size * sizeof(char), (size + 1) * sizeof(char));
+			if (new_hex == NULL)
+			{
+				free(hex);
+				return (-1);
+			}
+			hex = new_hex;
 			size++;
 			num /= 16;
 		}
 	}
 	hex[i] = '\0';
 	rev_string(hex);
-	*len += _putchar(hex[0]);
-	*len += _putchar(hex[1]);
+	_putchar(hex[0]);
+	_putchar(hex[1]);
 	free(hex);
-	return (0);
+	return (len);
 }
 
 /**
- * _non_printable - prints a sing with _non_printable charaterss
- * @s: our sing
+ * _non_printable - prints a string with non-printable characters
+ * @s: the string to print
  *
- * Return: actual sing length
+ * Return: the length of the string on success, or -1 on error
  */
 int _non_printable(char *s)
 {
-	int len = 0;
+	int len = 0, hex_len;
 	int c;
 
 	if (!s)
@@ -62,10 +70,15 @@ int _non_printable(char *s)
 	while (*s)
 	{
 		c = (unsigned char)*s;
-		if (c < 32 || c >= 127)
+		if (c < 32 || c >= 127 || (c >= 0x80 && c <= 0xFF))
 		{
 			len += write(1, "\\x", 2);
-			len += char_to_upper_hex(c, &len);
+			hex_len = char_to_upper_hex(c);
+			if (hex_len == -1)
+			{
+				return (-1);
+			}
+			len += hex_len;
 		}
 		else
 		{
